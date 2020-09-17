5507e56845b341ce5b515e0bc788d7c97ed047d4
valdeirandrade <andrade_mai@yahoo.com.br>
2020-09-17 09:16:12 -0400

    Criar Dao

--- Treinamento/src/Model/Agenda.java
+++ Treinamento/src/Model/Agenda.java
@@ -4,10 +4,12 @@ public class Agenda {
 	
 	private String nome;
 	private Integer numTelefone;
-	
-	
-	
-
+	private String endereçoEmail;
+	private String rua;
+	private Integer numeroCasa;
+	private String cidade;
+	private String estado;
+	private String cep;
 	public String getNome() {
 		return nome;
 	}
@@ -20,11 +22,53 @@ public class Agenda {
 	public void setNumTelefone(Integer numTelefone) {
 		this.numTelefone = numTelefone;
 	}
-	
-	
-	@Override
-	public String toString() {
-		return "nome=" + nome + ", numTelefone=" + numTelefone + "\n";
+	public String getEndereçoEmail() {
+		return endereçoEmail;
+	}
+	public void setEndereçoEmail(String endereçoEmail) {
+		this.endereçoEmail = endereçoEmail;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public Integer getNumeroCasa() {
+		return numeroCasa;
+	}
+	public void setNumeroCasa(Integer numeroCasa) {
+		this.numeroCasa = numeroCasa;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		if(cidade.length()==2) {
+		this.cidade = cidade.toLowerCase();
+		}
+		else {
+			System.out.println("Digitar somente siglas dos Estados");
+		}
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
 	}
 
-}
+	
+	
+	
+	
+
+	
+	}
+
--- Treinamento/src/Teste/TelaInicialAgenda.java
+++ Treinamento/src/Teste/TelaInicialAgenda.java
@@ -11,51 +11,8 @@ public class TelaInicialAgenda {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// List<Endereco> enderecos = new ArrayList<Endereco>();
-		List<Agenda> contatos = new ArrayList<Agenda>();
-		Scanner leia = new Scanner(System.in);
 		
-		Boolean lacoWhile = true;
-		System.out.println("**** LISTA DE CONTATOS****");
-		
-		while(lacoWhile==true) {
-		System.out.println("Digite <1> Adicionar contato, <2> Imprimir contatos, <3> Sair");
-		Integer opcao = leia.nextInt();
-		leia.nextLine();
-		Agenda novaAgenda = new Agenda();
-		
-		if(opcao==1) {
-			System.out.println("Digite o nome do contato: ");
-			novaAgenda.setNome(leia.nextLine());
-			System.out.println("Digite o numero do telefone: ");
-			novaAgenda.setNumTelefone(leia.nextInt());
-			leia.nextLine();
-			contatos.add(novaAgenda);
-		}
-		////itensA.forEach(item->System.out.println(item));
-		else if(opcao==2) {
-		System.out.println("imprimindo a contatos com to String \n ");
-		System.out.println(contatos.toString());
-		System.out.println("imprimindo contato com ForEach");
-		contatos.forEach(itemContato->System.out.println(itemContato));
-		//	
-		}
-		
-		else if(opcao==4){
-			lacoWhile=false;
-			System.out.println("fim");
-			
-		}
-		
-		else{
-			System.out.println("numero não valido");
-		}
-			
-			
-			
-			
-		}// fim while
-			
+	
 		
 		}
 		
