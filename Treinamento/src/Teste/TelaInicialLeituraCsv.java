1e43ad0d4dcc5f7798440d979ade9e6d4a375bca
valdeirandrade <andrade_mai@yahoo.com.br>
2020-10-15 10:07:52 -0400

    oi

---
+++ Treinamento/src/Teste/TelaInicialLeituraCsv.java
@@ -0,0 +1,49 @@
+package Teste;
+
+import java.util.List;
+
+import Controller.CsvController;
+import Controller.ViagemController2;
+import Dao.ViagemDao;
+import Model.Viagem;
+
+
+public class TelaInicialLeituraCsv {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CsvController csvController = new CsvController();
+		String caminhoArquivo = "C:\\csv\\ViagemTreinamento.csv";
+		
+		List<String[]> retornoListString = csvController.retornarCsv(caminhoArquivo);
+		
+		for(Integer i = 0; i < retornoListString.size(); i++) {
+				String[] itemList = retornoListString.get(i);
+			
+			//	for(Integer x = 0; x<itemList.length; x++) {
+					//System.out.println(itemList[x]);
+	//}
+				
+			ViagemController2 viagemController2 = new ViagemController2();
+				
+			Viagem novaViagem = new Viagem();
+			novaViagem = viagemController2.retornaObjetoViagem(itemList);
+			//System.out.println(novaViagem);
+			
+			ViagemDao viagemDao = new ViagemDao();
+			
+			viagemDao.inserirViagem(novaViagem);
+			
+		}
+		
+		ViagemDao viagemDao = new ViagemDao();
+		
+		List<Viagem> listaViagensBanco = viagemDao.retornarListViagem();
+		for(Integer i=0; i<listaViagensBanco.size(); i++) {
+			System.out.println(listaViagensBanco.get(i));
+			
+			
+		}
+	}
+
+}
--- Treinamento/src/Teste/TelaInicialViagem3.java
+++ Treinamento/src/Teste/TelaInicialViagem3.java
@@ -21,14 +21,24 @@ public class TelaInicialViagem3 {
 	String[] resultadoArray = viagemController3.retornaArrayString(novaViagem);
 	
 	
-	
+	//imprimindo array
 	System.out.println(resultadoArray[0]);
 	System.out.println(resultadoArray[1]);	
 	System.out.println(resultadoArray[2]);	
 	System.out.println(resultadoArray[3]);	
 	
+	String[] arrayStringViagem = new String[4];
+	arrayStringViagem[0] = "070";
+	arrayStringViagem[1] = "3";
+	arrayStringViagem[2] = "12h30";
+	arrayStringViagem[3] = "13h30";
 	
-		
+	viagemController3.retornaObjetoViagem(arrayStringViagem);
+	
+	
+	
+	
+	
 	}
 
 }
